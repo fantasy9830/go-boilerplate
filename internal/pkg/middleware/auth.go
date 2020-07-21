809c1f6942888f77b1d556f194171252efafbdc3
Ricky <rickylu@weintek.com>
2020-07-21 15:59:17 +0800

    update

--- internal/pkg/middleware/auth.go
+++ internal/pkg/middleware/auth.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"go-boilerplate/internal/app/models"
 	"go-boilerplate/internal/app/services"
 	"go-boilerplate/pkg/config"
 	"net/http"
@@ -13,64 +14,67 @@ import (
 // AuthRequired 認證JWT
 func AuthRequired() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		token, err := request.ParseFromRequestWithClaims(
-			ctx.Request,
-			request.OAuth2Extractor,
-			&jwt.StandardClaims{},
-			func(token *jwt.Token) (interface{}, error) {
-				return []byte(config.App.Key), nil
-			},
-		)
+		user, _, err := GetUserFromRequest(ctx.Request)
+		if err != nil {
+			// var validationError *jwt.ValidationError
+			// if errors.As(err, &validationError) {
+			// 	claims := token.Claims.(*jwt.StandardClaims)
 
-		if err == nil {
-			authServ := services.CreateAuthService()
-			user, err := authServ.GetUserFromToken(token.Raw)
-			if err != nil {
-				ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-					"error": err.Error(),
-				})
-				return
-			}
+			// 	refreshExpire := time.Unix(claims.IssuedAt, 0).Add((config.App.RefreshTTL * time.Second))
+			// 	if validationError.Errors == jwt.ValidationErrorExpired && time.Now().Before(refreshExpire) {
+			// 		u, err := user.GetUserFromToken(token.Raw)
+			// 		if err != nil {
+			// 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			// 				"error": err.Error(),
+			// 			})
+			// 			return
+			// 		}
 
-			ctx.Set("user", user)
-			ctx.Next()
+			// 		ctx.Set("user", u)
+			// 		refreshToken, err := auth.RefreshToken(token.Raw)
+			// 		if err != nil {
+			// 			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			// 				"error": err.Error(),
+			// 			})
+			// 			return
+			// 		}
+
+			// 		// TODO: Blacklisting old token
+
+			// 		ctx.Writer.Header().Add("Authorization", fmt.Sprintf("Bearer %s", refreshToken))
+			// 		ctx.Next()
+			// 		return
+			// 	}
+			// }
+
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": err.Error(),
+			})
 			return
 		}
 
-		// var validationError *jwt.ValidationError
-		// if errors.As(err, &validationError) {
-		// 	claims := token.Claims.(*jwt.StandardClaims)
-
-		// 	refreshExpire := time.Unix(claims.IssuedAt, 0).Add((config.App.RefreshTTL * time.Second))
-		// 	if validationError.Errors == jwt.ValidationErrorExpired && time.Now().Before(refreshExpire) {
-		// 		u, err := user.GetUserFromToken(token.Raw)
-		// 		if err != nil {
-		// 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-		// 				"error": err.Error(),
-		// 			})
-		// 			return
-		// 		}
-
-		// 		ctx.Set("user", u)
-		// 		refreshToken, err := auth.RefreshToken(token.Raw)
-		// 		if err != nil {
-		// 			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-		// 				"error": err.Error(),
-		// 			})
-		// 			return
-		// 		}
-
-		// 		// TODO: Blacklisting old token
-
-		// 		ctx.Writer.Header().Add("Authorization", fmt.Sprintf("Bearer %s", refreshToken))
-		// 		ctx.Next()
-		// 		return
-		// 	}
-		// }
-
-		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-			"error": err.Error(),
-		})
+		ctx.Set("user", user)
+		ctx.Next()
 		return
 	}
+}
+
+// GetUserFromRequest GetUserFromRequest
+func GetUserFromRequest(req *http.Request) (user *models.User, token *jwt.Token, err error) {
+	token, err = request.ParseFromRequestWithClaims(
+		req,
+		request.OAuth2Extractor,
+		&jwt.StandardClaims{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(config.App.Key), nil
+		},
+	)
+	if err != nil {
+		return
+	}
+
+	authServ := services.CreateAuthService()
+	user, err = authServ.GetUserFromToken(token.Raw)
+
+	return
 }
